ae6de19d11351c3ae7bab68595844506b8e0d570
Shamim Sorkar <shamimsorkar044@gmail.com>
2025-09-15 17:51:20 +0600

    Completed Hibernate CRUD

---
+++ Hibernate-CRUD/src/main/java/com/shamim/Hibernate_CRUD/entity/User.java
@@ -0,0 +1,24 @@
+package com.shamim.Hibernate_CRUD.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String university;
+}
